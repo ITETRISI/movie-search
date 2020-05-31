11a2c0f514246ec0abead66efbd5f91297938897
Daniil Asipenka <ocipenkoden@gmail.com>
2020-05-31 16:50:36 +0300

    feat: start search after the end of recognition

--- fancy-weather/src/js/microphone.js
+++ fancy-weather/src/js/microphone.js
@@ -1,16 +1,18 @@
+import location from './search-location';
+
 export default function inputByMicrophone(language) {
-	if (language === null) {
-		language = 'en';
-	}
-	const recognizer = new webkitSpeechRecognition();
-	recognizer.interimResults = true;
-	language += '-' + language.charAt(0).toUpperCase() + language.slice(1);
-	recognizer.lang = language;
-	recognizer.onresult = (event) => {
-		const result = event.results[event.resultIndex];
-		if (result.isFinal) {
-			document.querySelector('#input').value = result[0].transcript;
-		}
-	};
-	recognizer.start();
+  if (language === null) {
+    language = 'en';
+  }
+  const recognizer = new webkitSpeechRecognition();
+  recognizer.interimResults = true;
+  recognizer.lang = language;
+  recognizer.onresult = (event) => {
+    const result = event.results[event.resultIndex];
+    if (result.isFinal) {
+      document.querySelector('#input').value = result[0].transcript;
+      location.searchLocation(result[0].transcript, language);
+    }
+  };
+  recognizer.start();
 }
